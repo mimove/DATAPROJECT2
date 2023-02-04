47cccf8f8deccd704f81215114bee5b1ec24de3f
Jorge <martinezca.jorge@gmail.com>
2023-02-04 18:51:05 +0100

    Add Processing time to BQ Table

--- Dataflow/dataflow.py
+++ Dataflow/dataflow.py
@@ -33,14 +33,14 @@ def ParsePubSubMessage(message):
     #Return function
     return row
 
-#PTransform Classes
-# class getPowerPanel(beam.PTransform):
-#     def expand(self, pcoll):
-#         power_panel = (pcoll
-#             | "Pair keys" >> beam.Map(lambda x: (x,1))
-#             # CombinePerKey
-#             | "CombinePerKey" >> beam.CombinePerKey(sum))
-#         return power_panel
+# PTransform Classes
+class getPowerPanel(beam.PTransform):
+    def expand(self, pcoll):
+        power_panel = (pcoll
+            | "Pair keys" >> beam.Map(lambda x: (x,1))
+            # CombinePerKey
+            | "CombinePerKey" >> beam.CombinePerKey(sum))
+        return power_panel
 
 # DoFn Classes
 
@@ -54,82 +54,23 @@ class AddTimestampDoFn(beam.DoFn):
         #return function
         yield element
 
-# # DoFn 02 : Call GCP Data Loss Prevention
-# class DLPMaskingDataDoFn(beam.DoFn):
-#     """ Call API to simulate Data Loss Prevention Service."""
-#     #Initialize the class by setting the host and endpoint to call
-#     def __init__(self, hostname):
-#         self.hostname = hostname
-#         self.dlp_endpoint = '/dlp_masking'
-#     #Add process function
-#     def process(self, element):
-#         #Dealing with sensitive fields
-#         if element['Email'] != None:
-#             logging.info("Masking an Email field...")
-#             #Make API Request
-#             try:
-#                 api_request = requests.get(self.hostname + self.dlp_endpoint, params={"item": element['Email']})
-#                 #Show the status response in the logs
-#                 logging.info("Request was finished with the following status: %s", api_request.status_code)
-#                 #Dealing with API Response
-#                 element['Email'] = api_request.content.decode('utf-8')
-#                 logging.info(element)
-#                 yield element
-#             #Error handle
-#             except Exception as err:
-#                 logging.error("Error while trying to call to the API: %s", err)
-#         if element['Credit_Card_Number'] != None:
-#             logging.info("Masking a Credit_Card_Number field...")
-#             #Make API Request
-#             try:
-#                 api_request = requests.get(self.hostname + self.dlp_endpoint, params={"item": element['Credit_Card_Number']})
-#                 #Show the status response in the logs
-#                 logging.info("Request was finished with the following status: %s", api_request.status_code)
-#                 #Dealing with API Response
-#                 element['Credit_Card_Number'] = api_request.content.decode('utf-8')
-#                 yield element
-#             #Error handle
-#             except Exception as err:
-#                 logging.error("Error while trying to call to the API: %s", err)
-
-# # DoFn 03 : Call Machine Learning Model
-# class CallMLModelDoFn(beam.DoFn):
-#     """ Call API to simulate Machine Learning Model."""
-#     #Initialize the class by setting the host and endpoint to call
-#     def __init__(self, hostname):
-#         self.hostname = hostname
-#         self.ml_endpoint = '/ml_model'
-#     #Add process function
-#     def process(self, element):
-#         #Make API Request
-#         try:
-#             api_request = requests.get(self.hostname + self.ml_endpoint, json=element)
-#             #Show the status response in the logs
-#             logging.info("Request was finished with the following status: %s", api_request.status_code)
-#             #Dealing with API Response
-#             element['Transaction_Status'] = api_request.content.decode('utf-8')
-#             yield element
-#         #Error handle
-#         except Exception as err:
-#             logging.error("Error while trying to call to the API: %s", err)
-
 # DoFn 04: Dealing with frequent clients
 class getPanelsDoFn(beam.DoFn):
     def process(self, element):
         #Get Product_id field from the input element
         yield element['user_id']
 
-# DoFn 05 : Output data formatting
-class OutputFormatDoFn(beam.DoFn):
-    """ Set a specific format for the output data."""
-    #Add process function
-    def process(self, element):
-        count, user_id = element
-        #Send a notification with the best-selling product_id in each window
-        output_msg = {"ProcessingTime": str(datetime.now()), "message": f"{user_id} was the best-selling product."}
-        #Convert the json to the proper pubsub format
-        output_json = json.dumps(output_msg)
-        yield output_json.encode('utf-8')
+# # DoFn 05 : Output data formatting
+# class OutputFormatDoFn(beam.DoFn):
+#     """ Set a specific format for the output data."""
+#     #Add process function
+#     def process(self, element):
+#         count, panel_id = element
+#         #Send a notification with the best-selling product_id in each window
+#         output_msg = {"ProcessingTime": str(datetime.now()), "message": f"{panel_id} was the best-selling product."}
+#         #Convert the json to the proper pubsub format
+#         output_json = json.dumps(output_msg)
+#         yield output_json.encode('utf-8')
 
 """ Dataflow Process """
 def run():
@@ -145,10 +86,10 @@ def run():
                     '--input_subscription',
                     required=True,
                     help='PubSub Subscription which will be the source of data.')
-    parser.add_argument(
-                    '--output_topic',
-                    required=True,
-                    help='PubSub Topic which will be the sink for notification data.')
+    # parser.add_argument(
+    #                 '--output_topic',
+    #                 required=True,
+    #                 help='PubSub Topic which will be the sink for notification data.')
     parser.add_argument(
                     '--output_bigquery',
                     required=True,
@@ -183,6 +124,7 @@ def run():
                 | "Read From PubSub" >> beam.io.ReadFromPubSub(subscription=f"projects/{args.project_id}/subscriptions/{args.input_subscription}", with_attributes=True)
                 # Parse JSON messages with Map Function
                 | "Parse JSON messages" >> beam.Map(ParsePubSubMessage) 
+                | "Add Processing Time" >> beam.ParDo(AddTimestampDoFn())
         )
 
         """ Part 02: Writing data to BigQuery"""
@@ -194,6 +136,12 @@ def run():
                 write_disposition = beam.io.BigQueryDisposition.WRITE_APPEND
             )
         )
+        """ Part 03: Get Best-Selling product per Window and write to PubSub """
+        # (
+        #     data 
+        #         # Get Best-selling product
+        #         | "Get best-selling prduct" >> getPowerPanel()
+        # )
 
 if __name__ == '__main__':
     #Add Logs
