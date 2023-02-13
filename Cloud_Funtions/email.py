4bf15178853eb9b351486feb94d23275659a41b7
Jorge <martinezca.jorge@gmail.com>
2023-02-13 18:12:16 +0100

    Función Email Finalizada

--- Cloud_Funtions/email.py
+++ Cloud_Funtions/email.py
@@ -18,28 +18,28 @@ def send_email_alert(event, context):
 
     threshold_top = 200
 
-    if power > threshold_top:
-        if first_time_top:
-            first_time_top = False
-            print(f"Comienza la producción. La potencia es {power}")
+    if power > threshold_top and first_time_top:
 
-            to = "martinezca.jorge@gmail.com"
-            gmail_user = "pruebaemailsolar@gmail.com"
-            gmail_password = os.environ["GMAIL_PASSWORD"]
-            subject = "High value alert"
-            body = "Su instalación fotovoltaica ha comenzado a producir energía"
+        first_time_top = False
+        print(f"Comienza la producción. La potencia es {power}")
 
-            email_text = "From: {}\nTo: {}\nSubject: {}\n\n{}".format(gmail_user, to, subject, body)
+        to = "martinezca.jorge@gmail.com"
+        gmail_user = "pruebaemailsolar@gmail.com"
+        gmail_password = os.environ["GMAIL_PASSWORD"]
+        subject = "High value alert"
+        body = "Su instalación fotovoltaica ha comenzado a producir energía"
 
-            try:
-                server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-                server.ehlo()
-                server.login(gmail_user, gmail_password)
-                server.sendmail(gmail_user, to, email_text.encode('utf-8'))
-                server.close()
-                print("Production started. Email sent!")
-            except Exception as e:
-                print("Something went wrong: {}".format(e))
+        email_text = "From: {}\nTo: {}\nSubject: {}\n\n{}".format(gmail_user, to, subject, body)
+
+        try:
+            server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(gmail_user, to, email_text.encode('utf-8'))
+            server.close()
+            print("Production started. Email sent!")
+        except Exception as e:
+            print("Something went wrong: {}".format(e))
 
     if 5 <= power <= 10:
         if first_time_bottom and not first_time_top:
@@ -65,6 +65,3 @@ def send_email_alert(event, context):
                 print("Production finished. Email sent!")
             except Exception as e:
                 print("Something went wrong: {}".format(e))
-
-
-        
